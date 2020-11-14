2f6e2fe579d1dd195d5fde33d90ab227796de1e9
Filip Sądowicz <sadowicz31@gmail.com>
2020-11-14 17:49:04 +0100

    [red] capacity method returns RingBuffer capacity.

--- 02_tdd/02_exercise/project/tests/src/RingBufferTest.cpp
+++ 02_tdd/02_exercise/project/tests/src/RingBufferTest.cpp
@@ -10,6 +10,11 @@ TEST(RingBuffer, Constructor_PassCapacityThroughConstructor) {
     RingBuffer ringBuffer{5};
 }
 
+TEST(RingBuffer, Capacity_ReturnRingBufferCapacity) {
+    RingBuffer ringBuffer{5};
+    EXPECT_EQ(5, ringBuffer.capacity());
+}
+
 TEST(RingBuffer, Add) {
     RingBuffer ringBuffer{2};
     ringBuffer.add(1);
