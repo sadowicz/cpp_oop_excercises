28806b1d65d24d344fd9a4ebc4c37680a53699af
Filip Sądowicz <sadowicz31@gmail.com>
2020-11-30 19:00:31 +0100

    Large: Adding Equality operator benchmark.

--- 04_stl_benchmark/project/benchmarks/src/LargeBench.cpp
+++ 04_stl_benchmark/project/benchmarks/src/LargeBench.cpp
@@ -15,4 +15,19 @@ static void Large_LessThan(State& state) {
     }
 }
 
+static void Large_Equality(State& state) {
+
+    Large a{};
+    Large b{};
+
+    for(auto _ : state) {
+
+        a.randomize();
+        b.randomize();
+
+        a == b;
+    }
+}
+
 BENCHMARK(Large_LessThan);
+BENCHMARK(Large_Equality);
