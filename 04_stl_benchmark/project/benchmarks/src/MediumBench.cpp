5b151d4c1494bc7bb6f5785997a7cf80c6271929
Filip Sądowicz <sadowicz31@gmail.com>
2020-11-30 18:52:04 +0100

    Medium: Adding Equality operator benchmark.

--- 04_stl_benchmark/project/benchmarks/src/MediumBench.cpp
+++ 04_stl_benchmark/project/benchmarks/src/MediumBench.cpp
@@ -15,4 +15,19 @@ static void Medium_LessThan(State& state) {
     }
 }
 
+static void Medium_Equality(State& state) {
+
+    Medium a{};
+    Medium b{};
+
+    for(auto _ : state) {
+
+        a.randomize();
+        b.randomize();
+
+        a == b;
+    }
+}
+
 BENCHMARK(Medium_LessThan);
+BENCHMARK(Medium_Equality);
