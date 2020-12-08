ae74b0af5754d91dc874b80e9ae6fcd71738ec0b
Filip Sądowicz <sadowicz31@gmail.com>
2020-12-08 15:58:44 +0100

    Observer: Adding header and source files

---
+++ 05_design_patterns/02_exercise/project/include/observer/Observable.h
@@ -0,0 +1,8 @@
+//
+// Created by student on 12/8/20.
+//
+
+#ifndef PROJECT_OBSERVABLE_H
+#define PROJECT_OBSERVABLE_H
+
+#endif //PROJECT_OBSERVABLE_H
---
+++ 05_design_patterns/02_exercise/project/include/observer/Observer.h
@@ -0,0 +1,8 @@
+//
+// Created by student on 12/8/20.
+//
+
+#ifndef PROJECT_OBSERVER_H
+#define PROJECT_OBSERVER_H
+
+#endif //PROJECT_OBSERVER_H
